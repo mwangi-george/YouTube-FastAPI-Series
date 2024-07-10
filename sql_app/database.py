d4e028a9d45f5b5e8e5c2379c0e62ef77e81f6ea
George Mwangi <mwangigeorge648@gmail.com>
2024-07-10 14:51:18 +0300

    Database setup

---
+++ sql_app/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+# a database url --opening a file with the SQLite database
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+# Database connection engine
+engine = create_engine(
+    url=SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+# Database session class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for creating database models
+Base = declarative_base()
