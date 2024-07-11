1bc8a7aa0b9e5502b0e53e2aeafa0bb8abd8a407
George Mwangi <mwangigeorge648@gmail.com>
2024-07-11 10:28:51 +0300

    Final Routes

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,5 +1,5 @@
 from fastapi import Depends, FastAPI, HTTPException, status
-from sqlalchemy.orm import session
+from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
@@ -18,7 +18,7 @@ def get_db():
 
 
 @app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: session = Depends(get_db)):
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(
@@ -29,13 +29,13 @@ def create_user(user: schemas.UserCreate, db: session = Depends(get_db)):
 
 
 @app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: session = Depends(get_db)):
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db=db, skip=skip, limit=limit)
     return users
 
 
 @app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: session = Depends(get_db)):
+def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db=db, user_id=user_id)
     if db_user is None:
         raise HTTPException(
@@ -43,3 +43,14 @@ def read_user(user_id: int, db: session = Depends(get_db)):
             detail="User not found!"
         )
     return db_user
+
+
+@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+def create_item_for_user(user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+@app.get("/items", response_model=list[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db=db, skip=skip, limit=limit)
+    return items
