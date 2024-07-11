cb905f61b54e839c950d11bc0aebcb450cb35413
George Mwangi <mwangigeorge648@gmail.com>
2024-07-11 10:20:01 +0300

    Routes

--- sql_app/main.py
+++ sql_app/main.py
@@ -26,3 +26,20 @@ def create_user(user: schemas.UserCreate, db: session = Depends(get_db)):
             detail="User already registered"
         )
     return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: session = Depends(get_db)):
+    users = crud.get_users(db=db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: session = Depends(get_db)):
+    db_user = crud.get_user(db=db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found!"
+        )
+    return db_user
